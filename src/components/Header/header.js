c7e95ec3f9dc33d68ac04be21745061b26a4fc5a
Nikola <nikola96gospodinov@gmail.com>
2021-02-26 18:54:31 +0000

    Added a CTA

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -14,41 +14,48 @@ const Header = () => {
   const [nav, setNav] = useState(false)
 
   return (
-    <header>
-      <div class="header-outer">
-        <div className="header-inner">
-          <div className="header-inner-menu">
-              <FontAwesomeIcon onClick={() => setNav(!nav)} icon={['fas', 'bars']}/>
-            </div>
-            <div className="header-inner-social">
-              <a href="https://www.instagram.com/p.ch.production/" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={['fab', 'instagram']}/>
-              </a>
-              <a href="https://www.facebook.com/peter.chorbadzhiev" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={['fab', 'facebook']}/>
-              </a>
-              <a href="#" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={['fab', 'youtube']}/>
-              </a>
-            </div>
+    <>
+      <header>
+        <div class="header-outer">
+          <div className="header-inner">
+            <div className="header-inner-menu">
+                <FontAwesomeIcon onClick={() => setNav(!nav)} icon={['fas', 'bars']}/>
+              </div>
+              <div className="header-inner-social">
+                <a href="https://www.instagram.com/p.ch.production/" target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'instagram']}/>
+                </a>
+                <a href="https://www.facebook.com/peter.chorbadzhiev" target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'facebook']}/>
+                </a>
+                <a href="#" target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={['fab', 'youtube']}/>
+                </a>
+              </div>
+          </div>
         </div>
-      </div>
-      <Transition
-        items={nav}
-        from={{ marginLeft: -500 }}
-        enter={{ marginLeft: 0 }}
-        leave={{ marginLeft: -500 }}
-      >
-        {show => show && (props => <div style={props}><SideNav navigationSetter={setNav} navigationVar={nav}/></div>)}
-      </Transition>
+        <Transition
+          items={nav}
+          from={{ marginLeft: -500 }}
+          enter={{ marginLeft: 0 }}
+          leave={{ marginLeft: -500 }}
+        >
+          {show => show && (props => <div style={props}><SideNav navigationSetter={setNav} navigationVar={nav}/></div>)}
+        </Transition>
 
-      {nav ? <div onClick={() => setNav(!nav)} className="backdrop"></div> : null}
-      <div className="container">
-        <Link to="/">
-          <img src={Logo} alt="Logo"/>
+        {nav ? <div onClick={() => setNav(!nav)} className="backdrop"></div> : null}
+        <div className="container">
+          <Link to="/">
+            <img src={Logo} alt="Logo"/>
+          </Link>
+        </div>
+      </header>
+      <div className="bottom-right-cta">
+        <Link to="/contact">
+          <FontAwesomeIcon icon={['fas', 'envelope']}/> <span>Send me a message</span>
         </Link>
       </div>
-    </header>
+    </>
   )
 }
 
